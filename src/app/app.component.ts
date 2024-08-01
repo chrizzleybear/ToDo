7608750a0b99236deaf6dcff34fe338dfb486731
christopherstief <christopher.stief@student.uibk.ac.at>
2024-08-01 19:30:53 +0200

    added functionality for deleting items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,6 +40,7 @@ export class AppComponent implements OnInit {
 
     
     this.lastSavedItem = this.viewContainerRef.createComponent(ItemComponent);
+    this.lastSavedItem.instance.componentRef = this.lastSavedItem;
   }
 
   ngOnInit(): void {
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,12 +1,15 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ComponentRef } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, NgTemplateOutlet],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, NgTemplateOutlet, MatCardModule, MatButtonModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
@@ -15,11 +18,18 @@ export class ItemComponent {
     content: string = '';
     submitted: boolean = false;
 
+    componentRef!: ComponentRef<ItemComponent>
+
     onChange(event: Event){
         console.log('onChange has been called')
         let eventTarget = event.target as HTMLInputElement
         this.content = eventTarget.value;
         console.log(this.content)
+    }
+
+    deleteItem() {
+        console.log('destroying this Item')
+        this.componentRef.destroy();
     }
 
     onSaveInput() {
