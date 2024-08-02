273d902d89e9451ca399eff7ddff197b66147526
christopherstief <christopher.stief@student.uibk.ac.at>
2024-08-02 13:06:46 +0200

    added styling and check button

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,34 +1,53 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { Component, ComponentRef } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {FormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, NgTemplateOutlet, MatCardModule, MatButtonModule, FormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    NgTemplateOutlet,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css'
+  styleUrl: './item.component.css',
 })
 export class ItemComponent {
+  content: string = '';
+  submitted: boolean = false;
+  checked: boolean = false;
 
-    content: string = '';
-    submitted: boolean = false;
+  componentRef!: ComponentRef<ItemComponent>;
 
-    componentRef!: ComponentRef<ItemComponent>
+  deleteItem() {
+    console.log('destroying this Item');
+    this.componentRef.destroy();
+  }
 
-
-    deleteItem() {
-        console.log('destroying this Item')
-        this.componentRef.destroy();
-    }
-
-    onSaveInput() {
-        console.log('onSaveInput() has been called')
-        this.submitted = true;
-    }
+  checkItem() {
+    /* for now we are just destroying the component. But what we want to do is:
+    /  - introducing PostgREST
+    /  - storing the content of this item in the db
+    /  - build a simple routing mechanism to switch between active todos and checked todos
+    /  - maybe implement some statistics about how many items have been checked and when (with graphs)
+    */
+   
+    console.log('checking the Item')
+    this.componentRef.destroy();
+  }
+  onSaveInput() {
+    console.log('onSaveInput() has been called');
+    this.submitted = true;
+  }
 }
