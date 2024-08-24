ff35f30e838df4e2523b1d05e6d949351f9cab8d
christopherstief <christopher.stief@student.uibk.ac.at>
2024-08-24 11:10:49 +0200

    added some nice routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ItemComponent } from './item/item.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ActiveTodosComponent } from './active-todos/active-todos.component';
 import { CompletedTodosComponent } from './completed-todos/completed-todos.component';
+import { RouterLink } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    MatTabsModule,
+    RouterLink,
+    RouterLinkActive,
     RouterOutlet,
     ItemComponent,
     MatButtonModule,
@@ -20,4 +25,10 @@ import { CompletedTodosComponent } from './completed-todos/completed-todos.compo
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  links = [
+    { path: '/active-todos', name: 'Active Todos' },
+    { path: '/completed-todos', name: 'Completed Todos' },
+  ];
+  activeLink = { path: '/active-todos', name: 'Active Todos' };
+}
